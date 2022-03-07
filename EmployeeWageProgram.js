2aae0e52a68379b6f9de2e846bd0563a5217c51f
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-03-07 11:37:23 +0530

    [Raj] Added:- The code to calc emp wages till maxhours or month is reached

--- EmployeeWageProgram.js
+++ EmployeeWageProgram.js
@@ -9,16 +9,16 @@ const FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const NUM_OF_WORKING_DAYS = 20;
+const TOTAL_WORK_HOURS = 160;
 
 //Genrating random values
 let empCheck = Math.floor(Math.random() * 10) % 2;
 //Checking whether an employee is present or not(UC1)
 console.log((IS_ABSENT == empCheck) ? "Employee Is Absent" : "Employee Is Present")
 
-//Calculating wages for a month of an employee(UC4)
-let totalWage = 0;
-dailyEmpHrs = 0;
-for (let day = 1; day <= NUM_OF_WORKING_DAYS; day++) {
+let totalWage = 0,dailyEmpHrs = 0,day = 1,hours = 0;
+//Calculate Wages till a condition of total working hours of 160 or max days of 20 is reached for a month(UC && UC5)
+while (day <= NUM_OF_WORKING_DAYS && hours <= TOTAL_WORK_HOURS) {
     //Calculating daily employee wage and ref using function to check whether emp is present fulltime or parttime(UC2 && UC3)
     function getWorkingHours(empDailyCheck) {
         switch (empDailyCheck) {
@@ -36,6 +36,8 @@ for (let day = 1; day <= NUM_OF_WORKING_DAYS; day++) {
     dailyEmpHrs = getWorkingHours(empDailyCheck);
     let dailyEmpWage = dailyEmpHrs * WAGE_PER_HOUR;
     totalWage += dailyEmpWage;
-    console.log("Day: " + day + "\tToday Employee Is: " + employees[empDailyCheck] + "\tDailyWage: " + dailyEmpWage);
+    hours += dailyEmpHrs;
+    console.log("Day: "+day+"\tToday Employee Is:"+employees[empDailyCheck]+"\tEmp Total Work Hours: "+hours+" \tDailyWage:"+dailyEmpWage);
+    day++;
 }
-console.log("\nEmployee Total Wage Per Month is : " + totalWage)
+console.log("\nEmployee Total Wage For This Month is : " + totalWage)
