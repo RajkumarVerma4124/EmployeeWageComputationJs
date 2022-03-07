05f59d1032d69c8dd4f5fcafaea0196cb1ca3c37
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-03-07 10:08:49 +0530

    [Raj] Added:- The code to check emp attendence

--- EmployeeWageProgram.js
+++ EmployeeWageProgram.js
@@ -1,2 +1,9 @@
 //Displaying the welcome message
 console.log("\nWelcome To The Employee Wage Program");
+
+//Checking whether an employee is present or not(UC1)
+//Initializing constant
+const IS_ABSENT = 0;
+//Genrating random value either 0 or 1
+let empCheck = Math.floor(Math.random() * 10) % 2;
+console.log((IS_ABSENT == empCheck) ? "Employee Is Absent" : "Employee Is Present")
