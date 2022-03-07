07576eaae533fa3ea87294467760c17dab6dd725
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-03-07 10:42:52 +0530

    [Raj] Refactor:- To add function to get emp daily hours

--- EmployeeWageProgram.js
+++ EmployeeWageProgram.js
@@ -9,26 +9,24 @@ const FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 //Genrating random value either 0 or 1
-let empCheck = Math.floor(Math.random() * 10) % 2;
+let empCheck = Math.floor(Math.random() * 10) % 3;
 //Checking whether an employee is present or not(UC1)
 console.log((IS_ABSENT == empCheck) ? "Employee Is Absent" : "Employee Is Present")
 
-//Calculating daily employee wage(UC2)
-let empHrs = 0;
-let empDailyCheck = Math.floor(Math.random() * 10) % 3;
-//Initializin array with emp daily check
-let employees = ["Absent", "Part Time", "Full time"]
-console.log("Today Employee Is : " + employees[empDailyCheck]);
-switch (empDailyCheck) {
-    case PART_TIME:
-        empHrs = PART_TIME_HOURS;
-        break;
-    case FULL_TIME:
-        empHrs = FULL_TIME_HOURS;
-        break;
-    default:
-        empHrs = 0;
-        break;
+//Calculating daily employee wage and ref using function to check whether emp is present fulltime or parttime(UC2 && UC3)
+function getWorkingHours(empCheck) {
+    //Initializing array with emp daily check
+    let employees = ["Absent", "Part Time", "Full time"]
+    console.log("Today Employee Is : " + employees[empCheck]);
+    switch (empCheck) {
+        case PART_TIME:
+            return PART_TIME_HOURS;
+        case FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
 }
-let empWage = empHrs * WAGE_PER_HOUR;
+let empHours = getWorkingHours(empCheck);
+let empWage = empHours * WAGE_PER_HOUR;
 console.log("Employee Today's Wage Is : " + empWage);
