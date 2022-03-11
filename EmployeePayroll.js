7ed5d14a05384e533f2f4d7f911b8828ee4d450d
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-03-11 12:55:15 +0530

    [Raj] Added:- The code to validate employ properties

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -4,11 +4,6 @@ let empMap = new Map();
 
 //Created Employee Payroll class(UC11a)
 class EmployeePayroll {
-    //Declaring properties of class
-    id;
-    salary;
-    gender;
-    startDate;
 
     //Defining parameterizd constructor of class using constructor keyword(UC12)
     constructor(...parameters) {
@@ -28,8 +23,64 @@ class EmployeePayroll {
         else throw "Name Is Invalid";
     };
 
+    //Getter and setter methods for property of id(UC14)
+    get id() { return this._id };
+    set id(id)
+    {
+        //Pattern for id for positive numbers
+        let idPattern = new RegExp('^[1-9]*$');
+        if(idPattern.test(id))
+            this._id = id;
+        else
+            throw "The Given Id Is Invalid";
+    };
 
-    //Method to return string of values
+    //Getter and setter methods for property of salary(UC14)
+    get salary() { return this._salary };
+    set salary(salary) 
+    {
+        //Pattern for salary for positive numbers
+        let salaryPattern = new RegExp('^[1-9][0-9]*$');
+        if(salaryPattern.test(salary))
+            this._salary = salary;
+        else
+            throw "The Given Salary Is Invalid";
+         
+    };
+
+    //Getter and setter methods for property of gender(UC14)
+    get gender() { return this._gender };
+    set gender(gender)
+    {
+        //Pattern for gender either M or F
+        let genderPattern = new RegExp('M|F');
+        if(genderPattern.test(gender))
+            this._gender=gender;
+        else
+            throw "The Given Gender Is Not In Correct Format";
+    }
+
+    //Getter and setter methods for property of startdate(UC14)
+    get startDate(){ return this._startDate };
+    set startDate(date)
+    {
+        //Givendate should not exceed todays date
+        let todayDate = new Date().toLocaleDateString();
+        if(date <=todayDate)
+            this._startDate=date;
+        else
+            throw "The Given Date Is Greater Than Current Date";
+    }
+    //Getter and setter methods for property of name(UC13)
+    get name() { return this._name };
+    set name(name) {
+        let namePattern = RegExp('^[A-Z]{1}[a-z]{2,}$')
+        if (namePattern.test(name))
+            this._name = name
+        else throw "The Given Name Is Invalid";
+    };
+
+    //Method to return string of values(UC11)
     toString() {
         return `Id: ${this.id} \tName: ${this.name} \tSalary: ${this.salary} \tGender: ${this.gender} \tStartDate: ${this.startDate}`
     }
@@ -47,7 +98,7 @@ function addEmployee(id, name, salary, gender, startDate){
     return empMap;
 }
 
-//Function to add employee details from user
+//Function to add employee details from user(UC13)
 function addEmployeeDetailsFromUser(){
     try {
         let id = prompt("Enter Your Id : ");
@@ -63,7 +114,7 @@ function addEmployeeDetailsFromUser(){
     
 }
 
-//Function to add employee details from user
+//Function to add employee details from user(UC13)
 function upDateEmployeeDetailsFromUser(){
     try { 
         let id = parseInt(prompt("Enter The Id Of The Employee You Want To Edit : "));
@@ -94,15 +145,15 @@ function upDateEmployeeDetailsFromUser(){
     
 }
 
-//Creating obj for class using parameterized conbstructor
+//Creating obj for class using parameterized conbstructor(UC13)
 try {
     while(true)
     {
-        console.log("1: Enter Default Employee \n2: Enter EmployeeDetais By Yourself \n3: Update Employee \n4: Display Employee \n5: Exit");
+        console.log("1: Enter Default Employee \n2: Enter New Employee Details \n3: Update Employee \n4: Display Employee \n5: Exit");
         let choice = parseInt(prompt("Enter Your choice : "))
         switch(choice){
             case 1:
-                //Created obj for class using parameterized conbstructor
+                //Created obj for class using parameterized conbstructor(UC13)
                 addEmployee(1, "Rajkumar", 25000, 'M', new Date().toLocaleDateString());
                 addEmployee(2, "Mahipal", 11000, 'M', new Date().toLocaleDateString())
                 addEmployee(3, "Ajay" , 35000, 'M', new Date().toLocaleDateString())
